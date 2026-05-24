4f74bce3a91949f7b6996432801e654c8aba331d
AZIZ <asuyunov100@gmail.com>
2026-05-24 19:05:20 +0500

    fix: eslint warn only

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,13 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
 /** @type {import("eslint").Linter.Config[]} */
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+];
 
 export default eslintConfig;
